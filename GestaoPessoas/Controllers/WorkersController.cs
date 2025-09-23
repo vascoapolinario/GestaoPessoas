146abd92c8fd95965467ddb3e326c052c9001ddb
Vasco Apolinário <vascoapolinario@gmail.com>
2025-09-23 10:45:26 +0100

    Issue 7 - CreatedAtAction
    #7 Ok substituido por created at action que entrega o url para o get, nome da ação e worker criado.

--- GestaoPessoas/Controllers/WorkersController.cs
+++ GestaoPessoas/Controllers/WorkersController.cs
@@ -47,7 +47,8 @@ namespace GestaoPessoas.Controllers
         [HttpPost(Name = "AddWorker")]
         public ActionResult<Worker> AddWorker(Worker newworker)
         {
-            return Ok(WorkerService.AddWorker(newworker));
+            Worker? createdWorker = WorkerService.AddWorker(newworker);
+            return CreatedAtAction(nameof(GetWorker), new {id = createdWorker.Id}, createdWorker);
         }
 
         [HttpDelete("{id}", Name = "DeleteWorker")]
