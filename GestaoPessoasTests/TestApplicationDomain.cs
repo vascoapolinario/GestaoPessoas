b6c3f24622b61fee94b072a3f2f7a38e50ed793e
Vasco Apolinário <vascoapolinario@gmail.com>
2025-09-30 09:42:54 +0100

    Implementação de projeto testes (INCOMPLETO)
    #29 Criação do esqueleto para testes do projeto gestão pessoas

---
+++ GestaoPessoasTests/TestApplicationDomain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestaoPessoasTests
+{
+    internal class TestApplicationDomain
+    {
+        private ServiceProvider? serviceProvider = null;
+        private readonly ServiceCollection services;
+
+        internal ServiceCollection Services
+        {
+            get
+            {
+                if (serviceProvider != null) throw new Exception("Uma vez consultado o ServiceProvider já não é possível manipular a coleção de serviços. Se for preciso registar algum serviço faça antes de aceder ao ServiceProvider.");
+                return services;
+            }
+        }
+
+        public TestApplicationDomain() 
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<TestApplicationDomain>();
+            var configuration = configurationBuilder.Build();
+            services = new ServiceCollection();
+            services.AddScoped(f => configuration);
+        }
+    }
+}
---
+++ GestaoPessoasTests/Tests/WorkerServicePostGres.cs
@@ -0,0 +1,25 @@
+﻿using GestaoPessoas.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoPessoasTests.Tests
+{
+    [TestClass]
+    public sealed class WorkerServicePostGresTests : WorkerServiceTestsBase
+    {
+        private TestApplicationDomain? applicationDomain;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            applicationDomain = new TestApplicationDomain();
+            applicationDomain.Services.AddScoped<IWorkerService, WorkerServicePostGres>();
+            app
+           // WorkerServicePostGres WorkerService;
+        }
+    }
+}
